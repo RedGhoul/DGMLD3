ab1dd0382b181625186e2d66c38bc0e4acb9d17a
RedGhoul <avaneesab5@gmail.com>
2020-05-23 20:02:23 -0400

    Added new view models

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DGMLD3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/UploadViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGMLD3.Models
+{
+    public class UploadViewModel
+    {
+        public IFormFile file { get; set; }
+        public string DGML_Type_ID { get; set; }
+        public IEnumerable<SelectListItem> DGML_Types { get; set; }
+        public string ErrorMsg { get; set; }
+
+        public string GraphName { get; set; }
+        public UploadViewModel()
+        {
+            this.DGML_Types = new List<SelectListItem>()
+            {
+                new SelectListItem("CODE-MAP","CODE"),
+                new SelectListItem("DB-MAP","DB")
+            };
+        }
+    }
+}
