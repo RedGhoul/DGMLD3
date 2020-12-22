637d658392867715e2d722d1da06333d5d0d2f74
RedGhoul <avaneesab5@gmail.com>
2020-12-22 15:52:03 -0500

    Added pictures

--- Startup.cs
+++ Startup.cs
@@ -49,8 +49,9 @@ namespace DGMLD3
             services.AddSingleton<GraphRedisService, GraphRedisService>();
 
             services.AddControllersWithViews();
-            
-            
+            services.AddRazorPages()
+        .AddRazorRuntimeCompilation();
+
             services.AddResponseCaching();
             services.AddResponseCompression();
             services.AddTransient<UserManager<ApplicationUser>>();
